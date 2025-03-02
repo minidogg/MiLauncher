7368a072b01a2ee5c5349131a021196ca999dab9
minidogg <117037081+minidogg@users.noreply.github.com>
2025-03-02 12:52:57 -0600

    added button deployment thingy

---
+++ buttondeploy.js
@@ -0,0 +1,6 @@
+import path from 'path'
+import fs from 'fs'
+
+let bundleJS = fs.readFileSync("./dist/bundle.js", "utf-8")
+let outputButton = `<button onclick='let win = window.open("about:blank");win.eval(atob("${btoa(bundleJS)}"))'>Start</button>`
+fs.writeFileSync("./dist/button.txt", outputButton, "utf-8")
