1dcdeee07d72cdc9c7c0d7e57dcd5b5301aeab25
minidogg <117037081+minidogg@users.noreply.github.com>
2025-04-28 17:05:21 -0500

    button deploy

--- buttondeploy.js
+++ buttondeploy.js
@@ -2,5 +2,5 @@ import path from 'path'
 import fs from 'fs'
 
 let bundleJS = fs.readFileSync("./dist/bundle.js", "utf-8")
-let outputButton = `<button onclick='let win = window.open("https://google.com");win.eval(atob("${btoa(bundleJS)}"))'>Start</button>`
+let outputButton = `<button onclick='let win = window.open("");win.eval(atob("${btoa(bundleJS)}"))'>Start</button>`
 fs.writeFileSync("./dist/button.txt", outputButton, "utf-8")
